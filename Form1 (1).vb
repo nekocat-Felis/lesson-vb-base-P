7cfdaf75d11ba348e0d97e37c43cb5beabbb42a6
nekocat0320 <haradak0320@gmail.com>
2024-01-24 15:34:26 +0900

    commit

---
+++ Form1 (1).vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '2024124 nekocat-Felis 
+        Dim kai, n, d
+        Randomize()
+        kai = 0
+        For n = 1 To 600 Step 1
+            d = Int(Rnd() * 6 + 1)
+            If d = 1 Then
+                kai = kai + 1
+            End If
+        Next n
+        TextBox1.Text = kai
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        '2024124 nekocat-Felis 
+        Dim kai, n, d
+        Randomize()
+        n = 0
+        kai = 0
+        Do While kai < 100
+            n = n + 1
+            d = Int(Rnd() * 6 + 1)
+            If d = 1 Then
+                kai = kai + 1
+            End If
+        Loop
+        TextBox1.Text = n
+    End Sub
+End Class
